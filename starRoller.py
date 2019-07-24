c9c77556c38ea01bc384157f155664796669f1bd
mjperry82 <mjperry82@gmail.com>
2019-07-24 12:05:38 -0500

    Initial upload of main file and json config file.

---
+++ starRoller.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python3
+
+from tkinter import Tk, Label, Button
+from random import randrange
+from json import loads
+
+#load charachter properties from json file and decode them to char
+with open('starRoller.json', 'r') as f:
+    char_json = f.read()
+
+char = loads(char_json)
+
+skills = char.get('skills')
+abilities = char.get('abilities')
+damage = char.get('damage')
+saves = char.get('saves')
+
+BTN_PDING=1
+
+def rollDice(diceToRoll):
+    ## Takes a tuple with dice to roll formated {'d4': #, 'd6': #, 'd8': #, 'fixed': #}
+    ## Returns the total of rolled dice
+    diceTotal = 0
+    for dice in diceToRoll:
+        if dice == 'fixed':
+            diceTotal += diceToRoll.get(dice)
+        else:
+            count = 0
+            while count < diceToRoll.get(dice):
+                diceTotal += randrange(1, int(dice[1:])+1)
+                count += 1 
+    return diceTotal
+
+# create base tkinter window
+window = Tk()
+window.title("starRoller")
+
+# create label to indicate what dice roll is being displayed
+diceLabel = Label(window, text='', font=('Arial', 20), padx=5)
+diceLabel.grid(column=0, row=0, columnspan=3)
+
+# create label to display the rolled dice
+diceDisplay = Label(window, text="00", font=("Arial Bold", 50), padx=75)
+diceDisplay.grid(column=0, row=1, columnspan=3)
+
+critDisplay = Label(window, text='', font=("Arial Bold", 10), fg='red', padx=1, pady=1)
+critDisplay.grid(column=0, row=2, columnspan=3)
+
+### SKILLS buttons and function to roll skill checks
+
+def skillRoller(skill):
+    #print(skill)
+    diceRoll = randrange(1, 21)
+    skillResult = diceRoll + skills.get(skill)
+    if diceRoll == 20:
+        critDisplay.configure(text='Nat 20')
+    elif diceRoll == 1:
+        critDisplay.configure(text='Nat 1')
+    else:
+        critDisplay.configure(text='')
+    diceLabel.configure(text=skill.capitalize())
+    diceDisplay.configure(text=skillResult)
+
+skillButtons = []
+rowCount = 3
+for skill in skills:
+    if rowCount < 13:
+        column = 0
+        row = rowCount
+    else:
+        column = 1
+        row = rowCount - 10
+    rowCount += 1
+    b = Button(window, text='Roll {}'.format(skill.capitalize()), 
+               command=lambda skill=skill: skillRoller(skill))
+    b.grid(column=column, row=row, sticky='nsew', padx=BTN_PDING, pady=BTN_PDING)
+    skillButtons.append(b)
+
+### Attack Buttons
+
+def rollTAStealthClicked():
+    diceRoll = rollDice({'d20': 1})
+    taStealthRoll = diceRoll + skills.get('stealth') + 4
+    if diceRoll == 20:
+        critDisplay.configure(text='Nat 20')
+    elif diceRoll == 1:
+        critDisplay.configure(text='Nat 1')
+    else:
+        critDisplay.configure(text='')
+    diceLabel.configure(text='Trick Attack Stealth Roll')
+    diceDisplay.configure(text=taStealthRoll)
+
+taStealthButton = Button(window, text="Trick Attack Stealth", command=rollTAStealthClicked)
+taStealthButton.grid(column=2, row=3, sticky='nsew', padx=BTN_PDING, pady=BTN_PDING)
+
+def rollAttackClicked():
+    diceRoll = randrange(1, 21)
+    attackRoll = diceRoll + char.get('attack bonus')
+    if diceRoll == 20:
+        critDisplay.configure(text='Nat 20')
+    elif diceRoll == 1:
+        critDisplay.configure(text='Nat 1')
+    else:
+        critDisplay.configure(text='')
+    diceLabel.configure(text='Attack Roll')
+    diceDisplay.configure(text=attackRoll)
+
+attackButton = Button(window, text="Attack Roll", command=rollAttackClicked)
+attackButton.grid(column=2, row=4, sticky='nsew', padx=BTN_PDING, pady=BTN_PDING)
+
+# Damage Calculator Buttons
+
+def damageClicked(damageType):
+    damageRoll = rollDice(damage.get(damageType))
+    diceLabel.configure(text='{}'.format(damageType.capitalize()))
+    diceDisplay.configure(text=damageRoll)
+
+rowCount = 5
+damageButtons = []
+for dType in damage:
+    column = 2
+    row = rowCount
+    rowCount += 1
+    b = Button(window, text='{}'.format(dType.capitalize()), 
+               command=lambda dType=dType: damageClicked(dType))
+    b.grid(column=column, row=row, sticky='nsew', padx=BTN_PDING, pady=BTN_PDING)
+    damageButtons.append(b)
+
+# Saving Rolls
+
+def savingClicked(save):
+    diceRoll = randrange(1, 21)
+    saveResult = diceRoll + saves.get(save)
+    if diceRoll == 20:
+        critDisplay.configure(text='Nat 20')
+    elif diceRoll == 1:
+        critDisplay.configure(text='Nat 1')
+    else:
+        critDisplay.configure(text='')
+    diceLabel.configure(text='{} Save'.format(save.capitalize()))
+    diceDisplay.configure(text=saveResult)
+
+savingButtons = []
+for save in saves:
+    column = 2
+    row = rowCount
+    rowCount +=1
+    b = Button(window, text='{} Save'.format(save.capitalize()), 
+               command=lambda save=save: savingClicked(save))
+    b.grid(column=column, row=row, sticky='nsew', padx=BTN_PDING, pady=BTN_PDING)
+    savingButtons.append(b)
+window.mainloop()
